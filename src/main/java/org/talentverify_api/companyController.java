36f0231c27a86fb29db57e0c56bd3302192aa6d7
samuel23taku <tsmakadho@gmail.com>
2024-06-26 11:56:50 +0200

    First Commit

---
+++ src/main/java/org/talentverify_api/companyController.java
@@ -0,0 +1,12 @@
+package org.talentverify_api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// Adding,Removing companies etc
+@RestController
+@RequestMapping("/Company")
+public class companyController {
+
+}
